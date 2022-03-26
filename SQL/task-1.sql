b0016f87205e59bfdb198f7cc1ef13984499cba2
Mücahit Kökdemir <76880536+mucahitkok@users.noreply.github.com>
2022-03-27 00:14:01 +0300

    SECOND HOMEWORK COMPLETED

--- SQL/task-1.sql
+++ SQL/task-1.sql
@@ -8,6 +8,3 @@ SELECT * FROM film WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR rep
 SELECT last_name FROM customer WHERE first_name = 'Mary';
 --5
 SELECT * FROM film WHERE NOT length > 50 AND NOT (rental_rate = 2.99 OR rental_rate = 4.99);
-
-
-
