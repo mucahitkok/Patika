4a114251f7bec2136a09603c627519385affe433
Mücahit Kökdemir <76880536+mucahitkok@users.noreply.github.com>
2022-04-08 18:38:52 +0300

    Ödev tamamlandı

--- REACT/Testing Library/emoji-search-master/src/App.test.js
+++ REACT/Testing Library/emoji-search-master/src/App.test.js
@@ -1,8 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import { render, screen } from '@testing-library/react'
+import userEvent from "@testing-library/user-event";
+import '@testing-library/jest-dom'
+import React from 'react'
+import App from './App';
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+describe('Emoji testleri.', () => {
+  let input, emojiKiss;
+
+  beforeEach(() => {
+    render(<App />);
+    emojiKiss = screen.getByText(/Kissing Heart/i)
+    input = screen.getByPlaceholderText(/Emoji ara.../i)
+  })
+
+  test("Başlık kısmı.", () => {
+    const header = screen.getByText(/Emoji Search/i)
+    expect(header).toBeInTheDocument();
+  })
+
+  test("Emoji listesi.", () => {
+    expect(emojiKiss).toBeInTheDocument()
+  })
+
+  test("Filtreleme işlemi.", () => {
+    const emojiText = "100"
+    userEvent.type(input, emojiText)
+    expect(emojiKiss).not.toBeInTheDocument()
+  })
+
+  test("Copyalama işlemi.", () => {
+    document.execCommand = jest.fn();
+    userEvent.click(emojiKiss)
+    expect(document.execCommand).toBeCalledWith('copy')
+    const copyEmoji = window.ClipboardData;
+    expect(copyEmoji).toEqual(emojiKiss.value)
+  })
 });
